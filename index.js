7bc7a144311eb2bb60cae02af583d0f131e74e30
Claire Gallant <claire.luna.gallant@gmail.com>
2016-12-02 02:13:14 +0000

    updated a lot of the css, customized the html and played around with the server. Server still needs cleanup.

---
+++ index.js
@@ -0,0 +1,2 @@
+//this is the entry point for the express dependency
+//changed during init from (addressBook.js)
--- localServer8080.js
+++ localServer8080.js
@@ -1,38 +1,65 @@
-const http = require("http");
-const PORT = 8080;
+//this is the EXPRESS part of it all
+//you merged two documents - see if it still makes sense
+//https://codeforgeek.com/2015/01/render-html-file-expressjs/
 
-const express = require("express");
-const app = express();
-const path = require("path");
+// var http    = require("http");
+var express = require("express");
 
-// a function which handles requests and sends response
-function requestHandler(request, response) {
-  if (request.url == "/") {
-    response.end("Welcome!");
-  } else if (request.url == "/urls") {
-    response.end("www.lighthouselabs.ca\nwww.google.com");
-  } else if (request.url == "/addressBook") {
-    response.end("addressBook.html");
-  } else {
-    response.statusCode = 404;
-    response.end("404 error");
-  }
-}
+//     ERROR
+//     it cannot find module "express"
+//     do I need the entry point (index.js)?
+//     that's what I saved while installing express
 
-//viewed at http://localhost:8080
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname + "/addressBook.html"))
-})
+var app     = express();
+var path    = require("path");
+// var port    = 8080;
+// var server  = http.createServer(requestHandler);
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+app.get('/contacts',function(req,res){
+  res.sendFile(path.join(__dirname+'/addressBook.html'));
+});
+
+app.get('/new',function(req,res){
+  res.sendFile(path.join(__dirname+'/new.html'));
+});
+
+app.get('/edit',function(req,res){
+  res.sendFile(path.join(__dirname+'/edit.html'));
+});
 
 app.listen(8080);
 
-var server = http.createServer(requestHandler);
+console.log("Running at Port 8080");
 
-server.listen(PORT, () => {
-  console.log(`Server listening on: http://localhost:${PORT}`);
-});
+//---------------------------------------------------
+
+// a function which handles requests and sends response
+
+// function requestHandler(req, res) {
+//   if (request.url == "/") {
+//     response.end("Welcome!");
+//   } else if (request.url == "/urls") {
+//     response.end("www.lighthouselabs.ca\nwww.google.com");
+//   } else if (request.url == "/addressBook") {
+//     response.end("addressBook.html");
+//   } else {
+//     response.statusCode = 404;
+//     response.end("404 error");
+//   }
+// }
+
+// server.listen(PORT, () => {
+//   console.log(`Server listening on: http://localhost:${PORT}`);
+// });
 
 //hello world example from
+
+
 // ----------EXPRESSJS.COM-------------
 // var express = require('express')
 // var app = express()
